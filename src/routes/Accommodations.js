8419c44208bf139d24984eaf7ac6dbf7b2d1618f
serenahuang225 <serenahuang225@gmail.com>
2023-11-05 23:38:15 -0600

    fix accommodations

--- src/routes/Accommodations.js
+++ src/routes/Accommodations.js
@@ -104,61 +104,104 @@ Cour des Vosges
         </ul>
       </div>
 
-Also Great for Shopping: The 2nd / Sentier Neighborhood
+      <div className='full-container2 '>
+        <h4>Also Great for Shopping: The 2nd / Sentier Neighborhood</h4>
+        <p style={{marginBottom: "20px",marginTop: "10px"}}>
 Even though the 2nd Arrondissement is the smallest of Paris’s 20 districts and overlooked by many guidebooks, it actually packs in lots of interesting things to do and see into its small size. You can explore the city’s largest concentration of 19th-century historic glass-roofed arcades (Galerie Vivienne, the Passage du Grand-Cerf, and the Passage Choiseul), and dine in the popular Asian restaurants along Rue Sainte-Anne, and buy cheese, wine, and baguettes in the wonderful pedestrian-only Rue Montorgueil market street. Also, don’t forget to visit Stohrer, the oldest still-in-operation boulangerie in the entirety of the capital which has been in business since 1730 when it was founded by Louis XV’s pastry chef. 
-
+        </p>
+        <p style={{marginBottom: "20px",marginTop: "10px"}}>
 At the eastern end of the 2nd Arrondissement, you can explore the Sentier neighborhood, still filled with wholesale textile stores and garment manufacturers although they are now losing ground to hip bistros, bars, and upscale hotels. Sentier is definitely one of the best neighborhoods to experience Paris like a local Parisian.
-
-Where To Stay In The 2nd Arrondissement
-
-
-The Hoxton Hotel
+        </p>
+        <p>Where To Stay In The 2nd Arrondissement</p>
+        <ul>
+          <li>
+          The Hoxton Hotel
+          </li>
+          <li>
 Hôtel des Grands Boulevards
-La Maison Favart
-Hôtel du Sentier
+          </li>
+          <li>
+La Maison Favart</li>
+          <li>
+Hôtel du Sentier</li>
+          <li>
 Park Hyatt Paris Vendôme
+          </li>
+          <li>
 Hôtel Saint-Marc
-
-The 5th And 6th: Best Neighborhoods In Paris For Families
-The 6th Arrondissement is located on the Left Bank (“Rive Gauche”) and is home to the atmospheric Saint-Germain-des-Prés quarter, the Latin Quarter, and the Luxembourg Gardens. The 5th and 6th are highly central with great playgrounds and easily walkable with access to shops, sites, and restaurants. Family-friendly highlights include the National Museum of Natural History and the botanical garden known as Jardin des Plantes.
-
-The 5th is very central — maybe not as connected by metro — and offers a quieter, village-like feel. The 6th is more artistic and filled with art galleries and antique shops, as well as some of the city’s best small art museums. It’s also where you’ll find the impressive Musée de Cluny and two of Paris’ most famous cafés, Café de Flore and Les Deux Magots.
-
-Where To Stay In The 5th And 6th Arrondissements
-
-
-Hôtel Monte Cristo
-Hôtel Monge
-Relais Christine
-Hôtel Baume
-Hôtel D’Aubusson
+          </li>
+        </ul>
+      </div>
+      <div className='full-container2 '>
+        <h4>The 5th And 6th: Best Neighborhoods In Paris For Families</h4>
+        <p style={{marginBottom: "20px",marginTop: "10px"}}>
+        The 6th Arrondissement is located on the Left Bank (“Rive Gauche”) and is home to the atmospheric Saint-Germain-des-Prés quarter, the Latin Quarter, and the Luxembourg Gardens. The 5th and 6th are highly central with great playgrounds and easily walkable with access to shops, sites, and restaurants. Family-friendly highlights include the National Museum of Natural History and the botanical garden known as Jardin des Plantes.
+        </p>
+        <p style={{marginBottom: "20px",marginTop: "10px"}}>
+        The 5th is very central — maybe not as connected by metro — and offers a quieter, village-like feel. The 6th is more artistic and filled with art galleries and antique shops, as well as some of the city’s best small art museums. It’s also where you’ll find the impressive Musée de Cluny and two of Paris’ most famous cafés, Café de Flore and Les Deux Magots.
+        </p>
+        <p>Where To Stay In The 5th And 6th Arrondissements</p>
+        <ul>
+          <li>
+          Hôtel Monte Cristo
+          </li>
+          <li>
+          Hôtel Monge</li>
+          <li>Relais Christine</li>
+          <li>Hôtel Baume</li>
+          <li>Hôtel D’Aubusson
+          </li>
+          <li>
 Victoire & Germain
+          </li>
+        </ul>
+      </div>
+      <div className='full-container2 '>
+        <h4>The 9th And 18th: Most Romantic Neighborhoods In Paris</h4>
+        <p style={{marginBottom: "20px",marginTop: "10px"}}>
+        Lovers of French movie Amélie will adore Montmartre, Paris’s pretty 18th Arrondissement, home to many of the cult film’s quirky brasseries, parks, and cobbled streets (including Café des Deux Moulins at 15 rue Lepic). In the 19th century, Montmartre was a haunt for artists and bohemians including Monet, Renoir, Picasso, Van Gogh, and Toulouse-Lautrec. This neighborhood offers breathtaking views while retaining a quaint, village-like charm. And it’s the perfect place for a honeymoon visit or romantic outing. Don’t miss the Sacré Coeur, and enjoy one of the best views of Paris.
+        </p>
+        <p style={{marginBottom: "20px",marginTop: "10px"}}>
+        The 18th is bordered by the more central 9th Arrondissement, which is home to the historic Palais Garnier opera house, museums, and theaters. Also, the former red-light district Pigalle has reinvented itself, with trendy SoPi (South Pigalle) a great neighborhood for foodies. Don’t forget to wander in the street Rue Notre Dame de Lorette and to visit the Musée de la Vie Romantique.
+        </p>
+        <p>Where To Stay In The 9th And 18th Arrondissements</p>
+        <ul>
+          <li>1er Étage SoPi</li>
+          <li>Hôtel Adèle & Jules</li>
+          <li>Maison Souquet</li>
+          <li>Hôtel R de Paris</li>
+          <li>Le Relais Montmartre</li>
+          <li>Hôtel The Playce by Happyculture</li>
+        </ul>
+      </div>
+      <div className='full-container2 '>
+        <h4>The 8th, 15th, 16th, and 17th</h4>
+        <p style={{marginBottom: "20px",marginTop: "10px"}}>
+        Finally, the 8th, 15th, 16th, and 17th are great and safe Arrondissements, but these are more residential areas. That’s why I would not recommend these places to stay in Paris, especially if you want to be central and in a district easily walkable with access to shops, sites, and restaurants.
+        </p>
+      </div>
+      <div className='full-container2 '>
+        <h4>Areas To Avoid In Paris</h4>
+        <p style={{marginBottom: "20px",marginTop: "10px"}}>
+        Paris is generally a safe city. However, there are a few places I’ll recommend avoiding in Paris when you visit:
+        </p>
+        <ul>
+          <li>The Gare du Nord / Gare de l’Est area at night.</li>
+          <li>The area around Porte de Montreuil in the 20th district, in the Saint Blaise neighborhood at late hours.
+</li>
+          <li>Northern 18th and 19th district, specifically at night, around Marx Dormoy, Porte de la Chapelle, La Chappelle, Porte de Clignancourt, Porte de la Villette, Barbès, Goutte D’or, Chateau Rouge, Stalingrad. Montmartre is a very safe area day and night. Pigalle is safe too, there are a lot of tourists and police there, but maybe you can feel disturbed by the adult shops if you go there with kids.
+</li>
+          <li>Around Châtelet Les Halles at night.
+</li>
+          <li>The Bois de Boulogne: during the daytime, this park is perfectly safe because it is very active and crowded, but never under any circumstances should you go there at night. I would recommend that you avoid this area after dark as it has a reputation for being frequented by questionable characters.
+</li>
+        </ul>
+      </div>
 
-The 9th And 18th: Most Romantic Neighborhoods In Paris
-Lovers of French movie Amélie will adore Montmartre, Paris’s pretty 18th Arrondissement, home to many of the cult film’s quirky brasseries, parks, and cobbled streets (including Café des Deux Moulins at 15 rue Lepic). In the 19th century, Montmartre was a haunt for artists and bohemians including Monet, Renoir, Picasso, Van Gogh, and Toulouse-Lautrec. This neighborhood offers breathtaking views while retaining a quaint, village-like charm. And it’s the perfect place for a honeymoon visit or romantic outing. Don’t miss the Sacré Coeur, and enjoy one of the best views of Paris.
-
-The 18th is bordered by the more central 9th Arrondissement, which is home to the historic Palais Garnier opera house, museums, and theaters. Also, the former red-light district Pigalle has reinvented itself, with trendy SoPi (South Pigalle) a great neighborhood for foodies. Don’t forget to wander in the street Rue Notre Dame de Lorette and to visit the Musée de la Vie Romantique.
-
-Where To Stay In The 9th And 18th Arrondissements
 
 
-1er Étage SoPi
-Hôtel Adèle & Jules
-Maison Souquet
-Hôtel R de Paris
-Le Relais Montmartre
-Hôtel The Playce by Happyculture
 
-The 8th, 15th, 16th, and 17th
-Finally, the 8th, 15th, 16th, and 17th are great and safe Arrondissements, but these are more residential areas. That’s why I would not recommend these places to stay in Paris, especially if you want to be central and in a district easily walkable with access to shops, sites, and restaurants.
 
-Areas To Avoid In Paris
-Paris is generally a safe city. However, there are a few places I’ll recommend avoiding in Paris when you visit:
-The Gare du Nord / Gare de l’Est area at night.
-The area around Porte de Montreuil in the 20th district, in the Saint Blaise neighborhood at late hours.
-Northern 18th and 19th district, specifically at night, around Marx Dormoy, Porte de la Chapelle, La Chappelle, Porte de Clignancourt, Porte de la Villette, Barbès, Goutte D’or, Chateau Rouge, Stalingrad. Montmartre is a very safe area day and night. Pigalle is safe too, there are a lot of tourists and police there, but maybe you can feel disturbed by the adult shops if you go there with kids.
-Around Châtelet Les Halles at night.
-The Bois de Boulogne: during the daytime, this park is perfectly safe because it is very active and crowded, but never under any circumstances should you go there at night. I would recommend that you avoid this area after dark as it has a reputation for being frequented by questionable characters.
 
 
     </div>
